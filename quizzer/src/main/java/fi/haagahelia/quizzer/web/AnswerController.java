7bebd5abae8bc54075c09561c947db5a5034f6f0
kvilho <bhi06@myy.haaga-helia.fi>
2024-11-14 15:14:23 +0200

    Link to answerlist added

--- quizzer/src/main/java/fi/haagahelia/quizzer/web/AnswerController.java
+++ quizzer/src/main/java/fi/haagahelia/quizzer/web/AnswerController.java
@@ -1,10 +1,8 @@
 package fi.haagahelia.quizzer.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class AnswerController {
 
-    
 }
