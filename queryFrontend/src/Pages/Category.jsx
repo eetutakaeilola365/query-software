f79113d43d413a09bdec6c23d135a1e697d9afb7
Hilja Katajamäki <bhi033@myy.haaga-helia.fi>
2024-12-02 13:01:22 +0200

    CategoryQuizzes navigation

--- queryFrontend/src/Pages/Category.jsx
+++ queryFrontend/src/Pages/Category.jsx
@@ -1,16 +1,33 @@
-
+import { useNavigate } from 'react-router-dom'; // Importing useNavigate
 import { useState, useEffect } from 'react';
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-material.css"; // Optional Theme applied to the Data Grid
 import { getCategories } from '../../quizApi';
-
+import QuizList from './Quizlist';
 
 function Category() {
+  const navigate = useNavigate();
+
+  const handleCategoryClick = (categoryId) => {
+    navigate(`/category/${categoryId}/quizzes`); // Navigate to the quizzes for that category
+  };
+
   const [Categories, setCategories] = useState([]);
   const [colDefs, setColDefs] = useState([
-    { field: "name" },
+    { field: "name",
+      cellRenderer: params => (
+      <span
+        style={{ color: "#1976d2", textDecoration: "underline", cursor: "pointer" }}
+        onClick={() => handleCategoryClick(params.data.id)} // Handle category click
+      >
+        {params.value}
+      </span>
+    ),},
     { field: "description" },
+    {
+      cellRenderer: params => <QuizList data={params.data} handleFetch={handleFetch}></QuizList>, width: 120
+  },
   ])
 
   useEffect(() => {
@@ -38,7 +55,7 @@ function Category() {
       </header>
       <main style={styles.main}>
         <h2 style={styles.title}>Categories</h2>
-        <div className="ag-theme-material" style={{ height: 500 }}>
+        <div className="ag-theme-material" style={{height: 500 }}>
           <AgGridReact
             rowData={Categories}
             columnDefs={colDefs}
